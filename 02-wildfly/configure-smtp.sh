b62fe959d53bf25a2db59230ed6eafbd6a10eb7e
peder280370 <peder@carolus.dk>
2017-02-01 12:15:37 +0100

    Create custom managed executor service used for sending emails

--- 02-wildfly/configure-smtp.sh
+++ 02-wildfly/configure-smtp.sh
@@ -13,6 +13,9 @@ batch
 /socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=NiordSMTP:add(host=\${env.NIORDSMTP_PORT_25_TCP_ADDR:localhost}, port=\${env.NIORDSMTP_PORT_25_TCP_PORT:1025})
 /subsystem=mail/mail-session=NiordMail:add(jndi-name="java:jboss/mail/Niord", debug=false)
 /subsystem=mail/mail-session=NiordMail/server=smtp:add(outbound-socket-binding-ref=NiordSMTP)
+
+/subsystem=ee/managed-executor-service=MailExecutorService:add(jndi-name=java\:jboss\/ee\/concurrency\/executor\/MailExecutorService, core-threads=2, max-threads=2)
+
 run-batch
 stop-embedded-server
 EOF
