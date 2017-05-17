7052667c0f57e69de70b1fd3269be012ba8e7d1c
peder280370 <peder@carolus.dk>
2017-05-17 12:07:59 +0200

    Upgrade to Keycloak 3.1.0.Final

--- 03-keycloak/keycloak-env.sh
+++ 03-keycloak/keycloak-env.sh
@@ -4,7 +4,7 @@ pushd `dirname ${BASH_SOURCE}` > /dev/null
 export KEYCLOAK_CONF_DIR=`pwd`
 popd > /dev/null
 
-export KEYCLOAK_VERSION=2.5.1.Final
+export KEYCLOAK_VERSION=3.1.0.Final
 export KEYCLOAK=keycloak-$KEYCLOAK_VERSION
 export KEYCLOAK_ROOT=$KEYCLOAK_CONF_DIR/..
 export KEYCLOAK_PATH=$KEYCLOAK_ROOT/$KEYCLOAK
